a15e83ef1a78c34fa0cc526fe01af4b32569b3e3
pelin <kayhanpelin@gmail.com>
2019-11-10 11:57:21 +0000

    remove logo svg && add main element && total spending function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
 import Alert from "./components/Alert";
+import uuid from "uuid/v4";
 
 const initialExpenses = [
-  { id: 1, charge: "rent", amount: 1600 },
-  { id: 2, charge: "car payment", amount: 400 },
-  { id: 3, charge: "credit card bill", amount: 1200 }
+  { id: uuid(), charge: "rent", amount: 1600 },
+  { id: uuid(), charge: "car payment", amount: 400 },
+  { id: uuid(), charge: "credit card bill", amount: 1200 }
 ]
 
-
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+  //expenses first item in the array, setExpenses second item in the array which is function.
   return (
     <>
       <Alert />
-      <ExpenseForm />
-      <ExpenseList />
+      <h1>Budget Calculator</h1>
+      <main>
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>Total Spending :{}
+        <span className="total">
+          £ {" "}
+          {expenses.reduce((acc, num) => {
+            return (acc += num.amount);
+          }, 0)}
+        </span>
+      </h1>
+
     </>
   );
 }
